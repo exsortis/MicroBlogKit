f4c083e13d00b881294e003e3c2ece3a234e702d
Paul Schifferer <paul@schifferers.net>
2017-06-07 15:55:21 -0700

    Whitespace.

--- Sources/MicroBlogKit/Models/FeedItem.swift
+++ Sources/MicroBlogKit/Models/FeedItem.swift
@@ -17,29 +17,29 @@ public struct FeedItem {
     public let url : URL
     public let contentHTML : String
     public let datePublished : Date
-    public let extensions : [String : Any] 
+    public let extensions : [String : Any]
 }
 
 
 extension FeedItem : Serializable {
-
+    
     public init?(from json : JSONDictionary) {
         guard let id = json["id"] as? String,
-        let a = json["author"] as? [String : Any],
+            let a = json["author"] as? [String : Any],
             let author = Author(from: a),
-        let u = json["url"] as? String,
+            let u = json["url"] as? String,
             let url = URL(string: u),
-        let contentHTML = json["content_html"] as? String,
-        let d = json["date_published"] as? String,
-        let datePublished = dateFormatter.date(from: d)
+            let contentHTML = json["content_html"] as? String,
+            let d = json["date_published"] as? String,
+            let datePublished = dateFormatter.date(from: d)
             else { return nil }
-
+        
         self.id = id
         self.author = author
         self.url = url
         self.contentHTML = contentHTML
         self.datePublished = datePublished
-
+        
         if let e = json["_microblog"] as? [String : Any] {
             self.extensions = e
         }
@@ -47,7 +47,7 @@ extension FeedItem : Serializable {
             self.extensions = [:]
         }
     }
-
+    
     public func toDictionary() -> JSONDictionary {
         var dict : JSONDictionary = [
             "id" : id,
@@ -55,13 +55,13 @@ extension FeedItem : Serializable {
             "url" : url.absoluteString,
             "content_html" : contentHTML,
             "date_published" : dateFormatter.string(from: datePublished),
-        ]
-
+            ]
+        
         if self.extensions.count > 0 {
             dict["_microblog"] = extensions
         }
-
+        
         return dict
     }
-
+    
 }
